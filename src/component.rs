2d60f7cc158b769e8dd683d3ff247db7a07cf535
guilfoyles1 <157839471+guilfoyles1@users.noreply.github.com>
2024-10-18 13:14:39 -0400

    Update component.rs

--- src/component.rs
+++ src/component.rs
@@ -1,10 +1,13 @@
 use bevy::prelude::Component;
 
+// Represents the label of a button in the UI
 #[derive(Component)]
-pub struct ButtonLabel(pub String);
+pub struct ActionText(pub String);
 
+// Marker component for an interactive bubble element
 #[derive(Component)]
-pub struct Bubble;
+pub struct InteractiveBubble;
 
+// Marker component for displaying the current sequence of inputs
 #[derive(Component)]
-pub struct SequenceDisplay;
+pub struct InputDisplay;
