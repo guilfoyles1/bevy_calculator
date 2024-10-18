799f92707b5a8a569b76f51f4de080818ce3aa74
guilfoyles1 <157839471+guilfoyles1@users.noreply.github.com>
2024-10-18 13:11:28 -0400

    Create component.rs

---
+++ src/component.rs
@@ -0,0 +1,10 @@
+use bevy::prelude::Component;
+
+#[derive(Component)]
+pub struct ButtonLabel(pub String);
+
+#[derive(Component)]
+pub struct Bubble;
+
+#[derive(Component)]
+pub struct SequenceDisplay;
