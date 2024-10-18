2c7ad0e46569c450deb215cecb772e4d4974272a
guilfoyles1 <157839471+guilfoyles1@users.noreply.github.com>
2024-10-18 13:11:49 -0400

    Create click_button.rs

---
+++ src/click_button.rs
@@ -0,0 +1,12 @@
+use bevy::prelude::Resource;
+
+#[derive(Resource, Default)]
+pub struct ClickedButtons {
+    pub buttons: Vec<String>,
+}
+
+impl ClickedButtons {
+    pub fn to_number_string(&self) -> String {
+        self.buttons.join("") // Convert to a single string
+    }
+}
