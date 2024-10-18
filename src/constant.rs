9dd6695990288c299594a848ffbe1241c6d03211
guilfoyles1 <157839471+guilfoyles1@users.noreply.github.com>
2024-10-18 13:09:17 -0400

    Create constant.rs

---
+++ src/constant.rs
@@ -0,0 +1,8 @@
+use bevy::prelude::Color;
+
+pub const NORMAL_BUTTON: Color = Color::rgb(0.5, 0.0, 0.5); // Purple button color
+pub const HOVERED_BUTTON: Color = Color::rgb(0.7, 0.0, 0.7); // Lighter purple on hover
+pub const PRESSED_BUTTON: Color = Color::rgb(0.4, 0.0, 0.4); // Darker purple on press
+pub const PURPLE: Color = Color::rgb(0.5, 0.0, 0.5); // Border color
+pub const BACKGROUND_COLOR: Color = Color::rgb(1.0, 0.75, 0.8); 
+pub const BORDER_COLOR: Color = Color::WHITE;
